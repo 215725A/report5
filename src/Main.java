9dc4542b3da6a9e55bc2f7949c24e5b44b002473
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2021-12-18 12:26:51 +0900

    Step3 finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
+        /* Step1 ~ Step2
         String str = null;
         try {
             System.out.println(str.length());
@@ -7,5 +8,9 @@ public class Main {
             System.out.println("エラー発生");
             System.out.println(e.getMessage());
         }
+        */
+        // Step3
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
